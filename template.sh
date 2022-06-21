24bd5353f417b9bd97a82f72dfb9d426a86dcdf5
Ian Kersz <kerszamaral@gmail.com>
2022-06-21 15:22:18 -0300

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,38 +1,51 @@
-#Template script to download and install your custom version of vscode on inf ufrgs, be careful it wipes itself after closing vscode!
+#Script to download and install your custom version of vscode on inf ufrgs, be careful it wipes everything not commited after closing vscode!
 #Provide your own installation and codes!
 #Made by Ian Kersz Amaral 2022 
 #Need help? Contact me on discord: kerszamaral#7499
 
+#If the terminal outputs "permission denied" just type "chmod +x 'filename'.sh" and then run the script again!!
+
+#CREATING VSCODE PORTABLE FOR DOWNLOAD
+#Create a vscode portable version for linux and install all the extensions you want (you can login to your account and sync settings)
+#Enter the folder and move the file named "code" out of it.
+#Upload the entire folder to a github repository and set it to private.
+#Get the "code" file and upload it to OneDrive (it's the only service that can download it automatically from)
+#Click on the file on OneDrive and on "</>Embed", then copy the link inside first the quotes
+#On that link, change "embed" to "download" and you're done.
+
+#USERNAME
+#The username on the github links is your github username;
+
+#PASSWORD!!
+#The password you need to input in here is not your actual github password,
+#Go to github.com, login to your account, go to setting and "Developer settings"
+#Then, go to "personal access token" and generate a new token, it only need the "repo" scope.
+#It's advised to set the expiration date to "No expiration" to keep this script working forever, you can delete it later
+#The password will be stored in the .git folder temporarely, but after finishing this script the folders are wiped, so it won't be stored.
+
 #!/bin/bash
-sudo add-apt-repository universe && #adds universe repository to apt
-sudo apt-get update && #updates apt list
-sudo apt-get install build-essential gdb git python3-pip -y && #installs gcc, g++, gdb, git, and python-pip
-sudo pip install gdown && #installs gdown from pip 
-echo &&
-echo 'username: <your github username>' #username for your github repositories
-echo 'password: <your github personal access token with "repo" permission>' #password for your github repositories
-echo &&
-git clone <link your github repository with portable vscode linux> ./vscode_linux && #clones github repository with vscode for linux
-cd ./vscode_linux/VSCode-linux-x64 && #enter the vscode for linux folder to do next step
-gdown https://drive.google.com/uc?id=<id executable from google drive, dont forget to change it to public> && #downloads the code executable file from google drive
-chmod +x code && #grants running permissions to the executable file
-cd ../../ && #returns to the starting directory
-git clone <link your github repository with your codes> ./codes && #clones github repository with your coding files
-cd ./codes && #enter the coding files directory to start next step
-git config user.name "<your username>" && #adds your username for git commit
-git config user.email "<your email>" && #adds your email for git commit
-cd .. && #returns to the starting directory
-./vscode_linux/VSCode-linux-x64/code #starts vscode from executable
-#pauses while program is running, when it's closed, executes next steps
-cd ./codes && #enters the directory for the codes
-echo &&
-echo 'username: <your github username>' #username for your github repositories
-echo 'password: <your github personal access token with "repo" permission>' && #password for your github repositories
-echo &&
-git push origin main && #pushes the commit from vscode to the github repository
-cd .. && #returns to the starting directory
-sudo rm -rf ./codes && #erases the codes directory 
-sudo rm -rf ./vscode_linux && #erases the vscode install directory
-sudo pip uninstall gdown -y #uninstalls gdown from pip
-sudo apt-get remove build-essential gdb git python3-pip -y && #uninstalls gcc, g++, gdb, git, and python-pip
-sudo add-apt-repository --remove universe #removes universe repository from apt
+read -p "Do you want to install vscode? [y/n] " install
+if [[ "$install" == "y" || "$install" == "Y" ]]
+then
+	git clone https://username:password@github.com/username/repository ./vscode_linux && #clones private github repository with vscode for linux
+	cd ./vscode_linux/VSCode-linux-x64 && #enter the vscode for linux folder to do next step
+	wget --no-check-certificate -O code "your link for onedrive 'code' file" #downloads the code executable file from one drive
+	chmod +x code && #grants running permissions to the executable file
+	cd ../../ && #returns to the starting directory
+	git clone https://username:password@github.com/username/coderepository ./codes && #clones github repository with your coding files
+	cd ./codes && #enter the coding files directory to start next step
+	git config user.name "The name you want to appear on github history" && #adds your username for git commit
+	git config user.email "the email you want to appear on github history" && #adds your email for git commit
+	cd .. && #returns to the starting directory
+	./vscode_linux/VSCode-linux-x64/code -g ./codes #starts vscode from executable
+	#pauses while program is running, when it's closed, executes next steps
+fi
+read -p "Do you want to uninstall vscode? [y/n] " uninstall
+if [[ "$uninstall" == "y" || "$uninstall" == "Y" ]]
+then
+	cd ./codes && #enters the directory for the codes
+	git push origin main && #pushes the commit from vscode to the github repository
+	cd .. && #returns to the starting directory
+	rm -rf ./codes && #erases the codes directory 
+	rm -rf ./vscode_linux #erases the vscode install directory
+fi
